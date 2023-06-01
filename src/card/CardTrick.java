5961730df06e28d3c79e5c8cfa3799b10a046adc
pate5080 <133712406+pate5080@users.noreply.github.com>
2023-05-31 22:51:19 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -28,7 +28,7 @@ public class CardTrick {
         /*
          * the loop to randomly select a card from the deck of 52
          * There will be 7 cards selected in the magicHand
-        */
+         */
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
@@ -38,10 +38,16 @@ public class CardTrick {
             
         }
         
+        /*
+         * Programmer's lucky card
+         */
+        Card luckyCard = new Card();
+        luckyCard.setValue(7);
+        luckyCard.setSuit(Card.SUITS[2]);
         
         /*
          * Prompting the user to enter the card value and suit value
-        */
+         */
         System.out.print("Pick a card value (1-13): ");
         int pickedValue = sc.nextInt();
         
@@ -50,7 +56,7 @@ public class CardTrick {
         
         /*
          * Based on the choice, retriving the card
-        */
+         */
         Card pickedCard = new Card();
         pickedCard.setValue(pickedValue);
         pickedCard.setSuit(Card.SUITS[pickedSuit]);
@@ -59,7 +65,7 @@ public class CardTrick {
         
         /*
          * Checking for the card to be on of the card in the array
-        */
+         */
         for (Card card : magicHand) {
             if (card.getValue() == pickedCard.getValue() && card.getSuit().equals(pickedCard.getSuit())){
                 isCardFound = true;
@@ -69,13 +75,26 @@ public class CardTrick {
         
         /*
          * Printing out the result
-        */
+         */
         if (isCardFound) {
             System.out.println("Congratualations! Your card is in the magic hand.");
         } else {
             System.out.println("Sorry! Your card is not in the magic hand.");
         }
         
+        /*
+         * Comparing the lucky card to the picked card
+         */
+        if (luckyCard.getValue() == pickedCard.getValue() && luckyCard.getSuit().equals(pickedCard.getSuit())) {
+            System.out.println("You picked the lucky card!");
+        } else {
+            System.out.println("The lucky card is not the card you picked.");
+        }
+        
+        /*
+         * Printing the Lucky card and the choosen card
+         */
+        System.out.println("The lucky card is: " + luckyCard.getValue() + luckyCard.getSuit());
         System.out.println("Your card is: " + pickedCard.getValue() + pickedCard.getSuit());
     }
     
